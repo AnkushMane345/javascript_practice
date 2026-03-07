14535a32e9e653581bcacc9c2e8d709a73e7bcb0
AnkushMane345 <ankushmane3006@gmail.com>
2026-03-07 12:40:04 +0530

    Add files via upload

---
+++ js9.js
@@ -0,0 +1,21 @@
+let nan = "Ansh"
+// console.log(nan.length)
+console.log(nan[0])
+console.log(nan[1])
+console.log(nan[3])
+
+let friend='sahil'
+console.log(friend)
+
+//Template literals
+ let boy1='nikhil'
+ let boy2='aryan'
+ //i want to write "nikhil is friend of aryan"
+ let sentense =`${boy1} is a friend of ${boy2}`
+ console.log(sentense)
+ 
+ //Escape sequence character
+ let furit = 'bana\'na'
+ console.log(furit)
+ let furit1 = "bana\"na"
+ console.log(furit1)
